dee878758cfc656668882a53bd6f2f6c1ac1c8fa
Mahi <mahesh@westagilelabs.com>
2018-01-03 15:57:38 +0530

    Binding element

--- app/javascript/components/welcome/Show.vue
+++ app/javascript/components/welcome/Show.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 @click="printConsole" id="welcome">{{ message }}</h1>
+  <h1 @click="printConsole" id="welcome" :title="message2">{{ message }}</h1>
 </template>
 
 <script>
@@ -7,7 +7,10 @@
   export default {
     name: 'welcome-show',
     data: function(){
-      return { message: 'Hello User..!' }
+      return { 
+        message: 'Hello welcome to Vue JS..!',
+        message2: 'Thank you for hovering...!'
+      }
     },
     methods: {
       printConsole() {
