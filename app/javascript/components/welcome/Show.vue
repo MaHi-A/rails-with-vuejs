c938a4a1f2b035b83031b2003623cec137120d86
Mahi <mahesh@westagilelabs.com>
2018-01-08 17:36:39 +0530

    Using if else for showing and hiding the templates

--- app/javascript/components/welcome/Show.vue
+++ app/javascript/components/welcome/Show.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1 @click="printConsole" id="welcome" :title="message2">{{ message }}</h1>
+  <div>
+    <h1 @click="printConsole" id="welcome" :title="message2">{{ message }}</h1>
+    <div v-if="show">You can see this text</div>
+    <div v-if="dontHide"> You can't see me, as I'm hidden..!</div>
+  </div>
 </template>
 
 <script>
@@ -9,7 +13,9 @@
     data: function(){
       return { 
         message: 'Hello welcome to Vue JS..!',
-        message2: 'Thank you for hovering...!'
+        message2: 'Thank you for hovering...!',
+        show: true,
+        dontHide: false
       }
     },
     methods: {
